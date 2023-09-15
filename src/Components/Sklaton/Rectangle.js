1f5d9a679dfc971b0074669745db089571059cfa
Suvendu Chatterjee <107410530+cispl-suvendu@users.noreply.github.com>
2023-09-15 12:34:53 +0530

    skl

--- src/Components/Sklaton/Rectangle.js
+++ src/Components/Sklaton/Rectangle.js
@@ -3,7 +3,7 @@ import { Skeleton } from 'primereact/skeleton';
 export default function Rectangle() {
     return (
         <>
-            <Skeleton className="mb-2" height="2rem"></Skeleton>
+            <Skeleton className="mb-2 " height="2rem"></Skeleton>
             <Skeleton width="10rem" height="2rem" className="mb-2"></Skeleton>
             <Skeleton width="5rem" height="2rem" className="mb-2"></Skeleton>
             <Skeleton height="2rem" className="mb-2"></Skeleton>
